5f37161c79b48ae4ff853a9bf6899630064907c4
alexpozdnyakof <alexpozdnyakof@gmail.com>
2022-10-11 16:22:10 +0700

    feat: add new application props and html result

--- index.ts
+++ index.ts
@@ -1,3 +1,13 @@
-export default function App(){
- return 'Application' 
+type AppProps = {
+  title: string,
+  text: string
+}
+export default function App({title, text}: ){
+ return (
+  `<div>
+    <h1>${title}</h1>
+    <p>${text}</h1>
+    <button>Accept</button>
+  </div>`
+ ) 
 }
