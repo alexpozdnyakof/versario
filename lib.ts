fc2c273dfead3f213ba64105970eab11c408d299
alexpozdnyakof <alexpozdnyakof@gmail.com>
2022-10-11 17:41:34 +0700

    feat: add multiply and divide functions for -one -two -three

--- lib.ts
+++ lib.ts
@@ -2,4 +2,12 @@ function sum(x: number, y: number){
   return x + y
 }
 
+function multiply(x: number, y: number){
+  return x * y
+}
+
+function divide(x: number, y: number){
+  return x/y
+}
+
 export { sum } 
