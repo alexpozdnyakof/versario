4a5bdaddd4cd7d4fcbb969ee2444f9613af8dd43
alexpozdnyakof <alexpozdnyakof@gmail.com>
2022-11-01 15:48:20 +0700

    add doc

--- api.ts
+++ api.ts
@@ -1,4 +1,7 @@
+/**
+* Base Unit for application interaction
+**/
 export interface Unit<T,R> {
-  payload: (payload: T) => T;
-  callback: (result: R) => R
+    payload: (payload: T) => T;
+    callback: (result: R) => R;
 }
