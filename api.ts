47606ba4619fb5d50259976b07795985d0192b0a
alexpozdnyakof <alexpozdnyakof@gmail.com>
2022-11-08 00:12:09 +0700

    merge

--- api.ts
+++ api.ts
@@ -2,6 +2,8 @@
 * Base Unit for application interaction
 **/
 export interface Unit<T,R> {
+  extra: (extras: any) => void
   payload: (payload: T) => T;
   callback: (result: R) => R;
+  dataEnhance: (data: T) => Enhanced<T>;
 }
